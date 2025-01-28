4318f481decb0b8cbc77a4561837d2c5c0eee9ea
root <root@LAPTOP-33N12T6F.>
2025-01-28 15:10:23 -0500

    Addition of four numbers

--- addition_of_four_numbers.sh
+++ addition_of_four_numbers.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-Value_1=$1
-Value_2=$2
-Value_3=$3
-Value_4=$4
+#Calculate result from given numbers
+RESULT=$(./addition_of_four_numbers_correctly.sh 1 1 1 1)
 
-#Calculation of numbers and print it
-echo $((Value_1 + Value_2 + Value_3 + Value_4))
+echo "the result is: $RESULT"
+
+
---
+++ addition_of_four_numbers_correctly.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if the number of arguments is exactly 4
+if [ "$#" -eq 4 ]; then
+    echo "Error:4 arguments can't be taken."
+    exit 1
+fi
+
